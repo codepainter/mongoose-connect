3360c9ab40281f6e6f663f7260fa317a87bbd973
Abiyasa <abiyasa2410@gmail.com>
2020-04-25 18:54:24 +0700

    Fix: simplify fastify
    move default options to connect

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 const mongoose = require('mongoose')
-const fp = require('fastify-plugin')
-const { accessEnv } = require('../helpers')
 
 const log = require('debug')('libraries:mongoose')
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: true,
-  // reconnectTries    : Number.MAX_VALUE,   // Never stop trying to reconnect
-  // reconnectInterval : 500,                // Reconnect every 500ms
-  poolSize: 5, // Maintain up to 5 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0,
-  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
-  useUnifiedTopology: true
-}
 
 module.exports = {
-  fastify: fp(fastify),
+  fastify,
   express,
   mongoMemoryDB
 }
 
-function fastify (fastify, opts, next) {
+async function fastify (fastify, opts) {
   log('fastify:', opts)
-  const NODE_ENV = accessEnv('NODE_ENV', 'local')
+  let { environment, ...options } = opts
   const connection = connect({
-    ...opts,
-    debug: NODE_ENV !== 'production'
+    ...options,
+    debug: environment !== 'production'
   })
 
   fastify.addHook('onClose', (fastify, done) => connection.close(done))
@@ -39,12 +22,11 @@ function fastify (fastify, opts, next) {
     log(`Connected to: ${opts.uri} DB: ${opts.dbName}`)
   })
   fastify.decorate('mongo', connection)
-  next()
 }
 
 function express (opts, callback) {
   log('express:', opts)
-  const NODE_ENV = accessEnv('NODE_ENV', 'local')
+  const NODE_ENV = getEnv('NODE_ENV', 'local')
   const connection = connect({
     ...opts,
     debug: NODE_ENV !== 'production'
@@ -71,8 +53,23 @@ function mongoMemoryDB ({ dbName = undefined } = {}) {
 
 function connect ({ uri = 'mongodb://localhost:27017/test', dbName = 'test', debug = false }) {
   log('connect:', { uri, dbName, debug })
+  const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoIndex: true,
+    // reconnectTries    : Number.MAX_VALUE,   // Never stop trying to reconnect
+    // reconnectInterval : 500,                // Reconnect every 500ms
+    poolSize: 5, // Maintain up to 5 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4, // Use IPv4, skip trying IPv6
+    useUnifiedTopology: true
+  }
   mongoose.set('debug', debug)
-  mongoose.connect(uri, { dbName, ...options })
+  mongoose.connect(uri, { ...options, dbName })
   const db = mongoose.connection
   db.on('error', err => {
     log('Failed to connect to database', err)
